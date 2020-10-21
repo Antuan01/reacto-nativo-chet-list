647b30b8a738b22668d99d5a979c10afb4a6c0da
antuan01 <joseitox_4@gmail.com>
2020-10-20 23:57:44 -0400

    navigation

--- components/Root.jsx
+++ components/Root.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { StatusBar, ActivityIndicator, Button, Alert } from 'react-native';
+import { StatusBar, Button, Alert } from 'react-native';
 import Header from './Header';
 import Footer from './Footer';
+import Home from './Home';
 
 const Root = () => {
     return (
-        <View style={{paddingTop: StatusBar.currentHeight, justifyContent: 'space-between', flex: 1}} >
-            <Header />
-            <View style={{backgroundColor: 'powderblue', flex: 8}}>
-                <Text> app's sexy body </Text>
-                <Text> status bar: {StatusBar.currentHeight} </Text>
-                <Button
-                    onPress={() => Alert.alert('Fuiste Alertado')}
-                    title="Learn More"
-                    />
-            </View>
-            <View>
-                {/* <ActivityIndicator size={'large'} color={"green"}/> */}
-            </View>
-            <Footer />         
-        </View>
+        <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Header" component={Header} />
+        </Stack.Navigator>
     )
 }
 
