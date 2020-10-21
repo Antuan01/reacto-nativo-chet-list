258609abdcec9ac73f01426c60cb2d4446fe1645
antuan01 <joseitox_4@gmail.com>
2020-10-21 00:51:13 -0400

    navigation 2

--- App.js
+++ App.js
@@ -11,7 +11,8 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Home" component={Home} 
+                options={{headerStyle: {flex: 1}}} />
                 <Stack.Screen name="Header" component={Header} />
             </Stack.Navigator>
         </NavigationContainer>
